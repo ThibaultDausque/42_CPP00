d0628b29722d7a5d62147d21b5ce625d8ee0fb57
Thibault <thibault@macbookair.home>
2025-03-29 20:35:29 +0100

    add methods

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -19,17 +19,22 @@ int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initialDeposit)
 {
-	_displayTimestamp();
 	this->_accountIndex = _nbAccounts;
 	this->_nbAccounts++;
 	this->_nbWithdrawals = 0;
 	this->_amount = initialDeposit;
-	std::cout << ";created";
+	_displayTimestamp();
+  std::cout << " index:" << this->_accountIndex;
+  std::cout << ";amount:" << this->_amount;
+	std::cout << ";created" << std::endl;
 }
 
 Account::~Account(void)
 {
-
+  _displayTimestamp();
+  std::cout << " index:" << this->_accountIndex;
+  std::cout << ";amount:" << this->_amount;
+  std::cout << ";closed" << std::endl;
 }
 
 void	Account::_displayTimestamp(void)
@@ -41,3 +46,32 @@ void	Account::_displayTimestamp(void)
 	std::strftime(tab, 20, "[%Y%m%d_%H%M%S]", datetime);
 	std::cout << tab;
 }
+
+int Account::getNbAccounts(void)
+{
+  return (this->_nbAccounts);
+}
+
+int getTotalAmount(void)
+{
+  return (this->_totalAmount);
+}
+
+
+int Account::getNbDeposits(void)
+{
+  return (this->_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+  return (this->_nbWithdrawals);
+}
+
+void  Account::displayAccountsInfos(void)
+{
+
+}
+
+
+
